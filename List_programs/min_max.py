d603704a93d949c00251d7aeeba17ebe76b68c43
Joel Judish <147990619+Joel-Judish@users.noreply.github.com>
2025-01-28 14:51:32 +0530

    Delete List_programs directory

--- List_programs/min_max.py
+++
@@ -1,9 +0,0 @@
-n=int(input("Enter the limit:"))
-l=[]
-for i in range(n):
-    item=int(input("Enter the element:"))
-    l.insert(i,item)
-max=max(l)
-min=min(l)
-print("The maximum element is",max)
-print("The minimum element is",min)
