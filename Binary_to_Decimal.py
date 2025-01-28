f447ad96471297c0ffd53882a107d905874bea23
Joel Judish <147990619+Joel-Judish@users.noreply.github.com>
2025-01-28 14:50:58 +0530

    Delete Binary_to_Decimal.py

--- Binary_to_Decimal.py
+++
@@ -1,7 +0,0 @@
-bitString = input("Enter a String of bits: ")
-decimal = 0
-exponent = len(bitString)-1
-for digit in bitString:
-    decimal = decimal + int(digit) * 2 ** exponent
-    exponent = exponent - 1
-print("The Integer value is", decimal)
