d168234354c4bea599ec64821b08269cad7ead6f
Joel Judish <147990619+Joel-Judish@users.noreply.github.com>
2025-01-28 14:51:20 +0530

    Delete Dictionary_programs directory

--- Dictionary_programs/frequency.py
+++
@@ -1,9 +0,0 @@
-string= input("Enter a string: ")
-dict = string.lower().split()
-frequencies = {}
-for word in dict:
-    if word not in frequencies:
-        frequencies[word] = 1
-    else:
-        frequencies[word] += 1
-print("Word frequencies: ", frequencies)
